95410a6f5cf38e3301e029c281ebb55cad75da18
jiplaolm <mikel.larranaga@ehu.eus>
2023-01-24 18:35:25 +0100

    Ultimo mensaje

--- src/main/java/eus/ehu/eui/Main.java
+++ src/main/java/eus/ehu/eui/Main.java
@@ -2,6 +2,7 @@ package eus.ehu.eui;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Ultimo mensaje");
         System.out.println("Este es mi mensaje definitivo");
         System.out.println("Una versión alternativa");
     }
