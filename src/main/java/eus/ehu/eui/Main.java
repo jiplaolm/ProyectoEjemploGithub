28a4cdda26cc75e90e3f704675d20b16d69a0a44
jiplaolm <mikel.larranaga@ehu.eus>
2023-01-24 18:25:45 +0100

    Versión definitiva

--- src/main/java/eus/ehu/eui/Main.java
+++ src/main/java/eus/ehu/eui/Main.java
@@ -2,6 +2,6 @@ package eus.ehu.eui;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mi primer mensaje");
+        System.out.println("Este es mi mensaje definitivo");
     }
 }
